718af07b264aecf4d41dea0c6ad4c19a67740c43
Emily Domingos <emily.domingos@e-unicred.com.br>
2025-04-22 16:48:07 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { PhHouse } from "@phosphor-icons/vue";
+
+</script>
+
+<template>
+    <div class="bg-black h-screen">
+        <header class="flex justify-between w-full h-12 p-5">
+            <img src="/logo-header.svg" alt="Spotify logo" style="height: 35px;" />
+            <div>
+                <PhHouse class="text-white w-6 h-6" weight="fill"  />
+            </div>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        green: '#1ED760',
+        white: '#FFFFFF',
+        black: '#121212',
+      },
+      fontFamily: {
+        sans: [
+          'system-ui',
+          'Helvetica Neue',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
