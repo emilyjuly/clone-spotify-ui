9196090b5d8c29755e21df072f4eebed1ab867eb
Emily Domingos <emily.domingos@e-unicred.com.br>
2025-04-25 10:02:52 -0300

    Ajusta estilo do scrollbar

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,17 @@ import {
     PhListBullets,
 } 
 from "@phosphor-icons/vue";
+import { ref } from "vue";
 
 const tagsAside = ['Playslists', 'Artists', 'Albums', 'Podcasts & Shows']
 const libraryAside = ['Pop', 'Hiphop', 'Jazz', 'Blues', 'Pop up', 'Trending', 'Brazil', 'Pop', 'Hiphop', 'Jazz', 'Blues', 'Pop up', 'Trending', 'Brazil']
+const tagsMiddleSection = ['All', 'Music', 'Podcasts']
+
+const isScrolled = ref(false)
+
+const handleScroll = () => {
+    isScrolled.value = window.scrollY > 10
+}
 
 </script>
 
@@ -41,50 +49,79 @@ const libraryAside = ['Pop', 'Hiphop', 'Jazz', 'Blues', 'Pop up', 'Trending', 'B
                 </div>
             </div>
         </header>
-        <aside class="flex flex-col bg-[#121212] w-[20%] p-3 ml-2 rounded-1sm">
-            <div>
-                <div class="flex flex-col">
-                    <div class="flex justify-between items-center">
-                        <span class="text-[#ffff] font-bold text-[14px]">Your Library</span>
-                        <div class="flex items-center gap-3">
-                            <div class="bg-[#1f1f1f] rounded-full px-4 py-2 flex items-center justify-center gap-2">
-                                <PhPlus class="text-[#ffff]" />
-                                <span class="text-[#ffff] font-bold text-[13px]">Create</span>
+        <main class="flex">
+            <aside class="flex flex-col bg-[#121212] w-[20%] p-3 ml-2 rounded-1sm">
+                <div>
+                    <div class="flex flex-col">
+                        <div class="flex justify-between items-center">
+                            <span class="text-[#ffff] font-bold text-[14px]">Your Library</span>
+                            <div class="flex items-center gap-3">
+                                <div class="bg-[#1f1f1f] rounded-full px-4 py-2 flex items-center justify-center gap-2">
+                                    <PhPlus class="text-[#ffff]" />
+                                    <span class="text-[#ffff] font-bold text-[13px]">Create</span>
+                                </div>
+                                <PhArrowsOutSimple class="text-[#ffff]" />
                             </div>
-                            <PhArrowsOutSimple class="text-[#ffff]" />
                         </div>
                     </div>
+                    <div class="flex justify-between mt-5">
+                        <template v-for="tag in tagsAside">
+                            <div class="bg-[#1f1f1f] rounded-full px-3 py-1 flex items-center justify-center">
+                                <span class="text-[#ffff] text-[13px]">{{ tag }}</span>
+                            </div>
+                        </template>
+                    </div>
                 </div>
-                <div class="flex justify-between mt-5">
-                    <template v-for="tag in tagsAside">
-                        <div class="bg-[#1f1f1f] rounded-full px-3 py-1 flex items-center justify-center">
+                <div class="max-h-170 overflow-y-auto pr-5 mt-5">
+                    <div class="flex justify-between text-[#b3b3b3] items-center mt-5">
+                        <PhMagnifyingGlass :size="17" />
+                        <div class="flex items-center gap-2">
+                            <span class="text-[13px]">Recents</span>
+                            <PhListBullets />
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-4 mt-10">
+                        <template v-for="(library, i) in libraryAside">
+                            <div class="flex">
+                                <div class="rounded-md w-15 h-15 overflow-hidden">
+                                    <img :src="`/covers/cover${i + 1}.png`" />
+                                </div>
+                                <div class="flex flex-col ml-3 justify-center">
+                                    <span class="text-[#ffff] text-md">{{ library }}</span>
+                                    <span class="text-[#b3b3b3] text-[12px]">Playlist</span>
+                                </div>
+                            </div>
+                        </template>
+                    </div>
+                </div>
+            </aside>
+            <section class="flex flex-col bg-[#121212] w-[50%] p-3 ml-2 rounded-1sm">
+                <div class="flex gap-2 p-3" :class="['top-bar', { 'scrolled': isScrolled }]">
+                    <template v-for="tag in tagsMiddleSection">
+                        <div class="bg-[#1f1f1f] rounded-full px-4 py-2 flex items-center justify-center">
                             <span class="text-[#ffff] text-[13px]">{{ tag }}</span>
                         </div>
                     </template>
                 </div>
-            </div>
-            <div class="max-h-170 overflow-y-auto pr-5 mt-5">
-                <div class="flex justify-between text-[#b3b3b3] items-center mt-5">
-                    <PhMagnifyingGlass :size="17" />
-                    <div class="flex items-center gap-2">
-                        <span class="text-[13px]">Recents</span>
-                        <PhListBullets />
-                    </div>
-                </div>
-                <div class="flex flex-col gap-4 mt-10">
-                    <template v-for="(library, i) in libraryAside">
-                        <div class="flex">
-                            <div class="rounded-md w-15 h-15 overflow-hidden">
-                                <img :src="`/covers/cover${i + 1}.png`" />
-                            </div>
-                            <div class="flex flex-col ml-3 justify-center">
-                                <span class="text-[#ffff] text-md">{{ library }}</span>
-                                <span class="text-[#b3b3b3] text-[12px]">Playlist</span>
-                            </div>
-                        </div>
-                    </template>
-                </div>
-            </div>
-        </aside>
+            </section>
+        </main>
     </div>
 </template>
+
+<style scoped>
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #555;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #888;
+}
+</style>
